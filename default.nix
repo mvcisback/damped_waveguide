cd3061d6a04dcdb06bffb163fa97fa73c91f5a8f
Marcell Vazquez-Chanlatte <mvc@linux.com>
2015-05-02 17:00:49 -0500

    start actual work on paper

--- default.nix
+++ default.nix
@@ -1,25 +1,6 @@
 let
     pkgs = import <nixpkgs> {};
     stdenv = pkgs.stdenv;
-    lib = pkgs.haskell-ng.lib;
-    env = pkgs.haskellngPackages.ghcWithPackages (p: with p; [
-      ihaskell
-      ihaskell-charts
-      ihaskell-diagrams
-      dimensional-tf
-      statistics
-      cassava
-      hmatrix
-      numeric-tools
-      integration
-      numeric-prelude
-    ]);
-    ihaskellSh = pkgs.writeScriptBin "ihaskell-notebook" ''
-    #!/bin/sh
-    export GHC_PACKAGE_PATH="$(echo ${env}/lib/*/package.conf.d| tr ' ' ':'):$GHC_PACKAGE_PATH"
-    export PATH="${pkgs.ihaskell}/bin:${env}/bin:${pkgs.python27Packages.ipython}/bin"
-    ${pkgs.ihaskell}/bin/ihaskell install -l $(${pkgs.env}/bin/ghc --print-libdir) && ${pkgs.python27Packages.ipython}/bin/ipython notebook --kernel=haskell
-      '';    
  in rec {
      thesisEnv = pkgs.myEnvFun {
        name = "thesis-env";
@@ -27,7 +8,6 @@ let
             git
             haskellPackages.pandoc
             haskellPackages.pandocCiteproc
-            env
             python27
             python27Packages.sympy
             python27Packages.pillow
@@ -47,7 +27,7 @@ let
                 pkgs.lmodern
                 pkgs.texLiveCMSuper
             ]; })
-
+            (import ./languagetool.nix)
         ];
      };
 }
---
+++ paper/thesis.md
@@ -0,0 +1,21 @@
+---
+title: TODO
+author: Marcell Vazquez-Chanlatte
+abstract: TODO
+---
+
+# Introduction
+
+# Design of Drumpad & Instrumentation
+
+# Analysis of Drumpad Properties
+
+# Calibration Phase
+
+# Localization
+
+# Issues and Potential Solutions
+
+# Future Work
+
+# Conclusion
